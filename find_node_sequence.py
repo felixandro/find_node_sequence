3cb1e5ca57f18f4e672a47d329614ee5a794d75e
VyR-10 <felixandro>
2024-12-29 17:53:29 -0300

    Se agregó el algoritmo find_node_sequence y el st_main.py que es el punto de entrada a la aplicación

---
+++ find_node_sequence.py
@@ -0,0 +1,120 @@
+########################
+#####  Frameworks  #####
+########################
+
+import networkx as nx
+from shapely.geometry import Point
+
+
+##################################
+#####  Funciones Auxiliares ######
+##################################
+
+#prepare_nodes_gdf: GeoDataFrame Str -> GeoDataFrame
+#Configura los id de los nodos como los indices del GeoDataFrame
+def prepare_nodes_gdf(nodes_gdf, node_id_col):
+    nodes_gdf_copy = nodes_gdf.copy()
+    return nodes_gdf_copy.set_index(node_id_col)
+
+#add_distance_between_nodes_and_linestring: LineString GeoDataFrame -> GeoDataFrame
+#Se agrega la distancia entre los nodos y la línea de referencia
+#como una columna adicional al GeoDataFrame de nodos
+def add_distance_col_to_nodes(linestring, nodes_gdf):
+    nodes_gdf_copy = nodes_gdf.copy()    
+    nodes_gdf_copy["d_to_ls"] = nodes_gdf_copy.geometry.distance(linestring) 
+    #d_to_ls es abreviación de distance_to_linestring
+    return nodes_gdf_copy
+
+#add_weight_col_to_edges: GeoDataFrame GeoDataFrame Str Str -> GeoDataFrame
+#A cada arco le agrega su peso como el promedio de las distancias 
+#entre la línea de referencia y los nodos que lo definen
+def add_weight_col_to_edges(nodes_gdf, 
+                            edges_gdf, start_node_col, end_node_col):    
+    weight_list = []
+    for _, row in edges_gdf.iterrows():
+        #Identificadores Nodos de inicio y fin
+        start_node = row[start_node_col]
+        end_node = row[end_node_col]
+        #Distancia desde linea de referencia a Nodos de inicio y fin
+        start_node_distance = nodes_gdf.loc[start_node,"d_to_ls"] #Distancia Nodo Inicio a Linea de Referencia
+        end_node_distance = nodes_gdf.loc[end_node,"d_to_ls"] #Distancia Nodo Fin a Linea de Referencia
+        #Peso Arco
+        weight = (start_node_distance + end_node_distance) / 2
+        weight_list.append(weight)
+    edges_gdf_copy = edges_gdf.copy()
+    edges_gdf_copy["weight"] = weight_list
+    return edges_gdf_copy        
+
+#build_network: GeoDataFrame Str Str Str -> nx.DiGraph
+#Construye el grafo dirigido referente a edges_gdf, el cual debe tener una columna con pesos
+def build_network(edges_gdf, start_node_col, end_node_col):
+    network = nx.DiGraph() #Grafo Dirigido Vacío
+    for _, row in edges_gdf.iterrows():
+        start_node = row[start_node_col]
+        end_node = row[end_node_col]
+        weight = row["weight"]
+        network.add_edge(start_node, end_node, weight = weight)
+    return network
+
+#first_last_point: LineString Str -> Point Point
+def first_last_point(linestring,direction):
+    #Validar variables de entrada
+    assert linestring.geom_type == "LineString" 
+    assert direction == "NS" or direction == "SN"
+    first_point_tuple = list(linestring.coords)[0]
+    last_point_tuple = list(linestring.coords)[-1]
+    if first_point_tuple[1] > last_point_tuple[1]: #CASO DIBUJO NS 
+        if direction == "SN":
+            first_point_tuple, last_point_tuple = (last_point_tuple, first_point_tuple)
+    else: #CASO DIBUJO SN 
+        if direction == "NS":
+            first_point_tuple, last_point_tuple = (last_point_tuple, first_point_tuple)
+    first_point = Point(first_point_tuple)
+    last_point = Point(last_point_tuple)
+    return first_point, last_point
+
+#find_nearest_node_id: Point GeoDataFrame -> Int
+#Entrega el id del punto más cercano a otro de referencia
+def find_nearest_node_id(point, nodes_gdf):
+   geometry_serie = nodes_gdf.geometry
+   distance_to_point_serie = geometry_serie.distance(point)
+   nearest_node_id = distance_to_point_serie.idxmin()
+   return nearest_node_id
+
+#get_min_route: nx.DiGraph Str int int -> list[int]
+#Entrega la secuencia de nodos respectiva a la ruta mínima 
+#en la red de arcos ponderados desde un nodo fuente a otro sumidero
+def get_min_route(network, source_node, sink_node):
+    if nx.has_path(network, source = source_node , target = sink_node):
+      node_sequence = nx.shortest_path(network, source = source_node, target = sink_node, weight = "weight")
+    else:
+      node_sequence = []
+    return node_sequence
+
+
+#######################################
+#######  Función Consolidadada  #######
+#######################################
+
+#find_node_sequence: LineString GeoDataFrame Str Str Str Str Str Str -> list[int]
+#Encuentra la secuencia de nodos que minimiza la distancia entre la línea de referencia y los nodos
+def find_node_sequence(linestring, route_direction,
+                       nodes_gdf, node_id_col,
+                       edges_gdf, start_node_col, end_node_col):
+    #Preparar GeoDataFrame de Nodos
+    nodes_gdf_prepared = prepare_nodes_gdf(nodes_gdf, node_id_col)
+    #Agregar Distancia entre Nodos y Linea de Referencia
+    nodes_gdf_with_distance = add_distance_col_to_nodes(linestring, nodes_gdf_prepared)
+    #Agregar Peso a los Arcos
+    edges_gdf_with_weight = add_weight_col_to_edges(nodes_gdf_with_distance, 
+                                                    edges_gdf, start_node_col, end_node_col)
+    #Construir Red
+    network = build_network(edges_gdf_with_weight, start_node_col, end_node_col)
+    #Puntos Iniciales y Finales
+    first_point, last_point = first_last_point(linestring, route_direction)
+    #Nodos Iniciales y Finales
+    source_node_id = find_nearest_node_id(first_point, nodes_gdf_with_distance)
+    sink_node_id = find_nearest_node_id(last_point, nodes_gdf_with_distance)
+    #Secuencia de Nodos
+    node_sequence = get_min_route(network, source_node_id, sink_node_id)
+    return node_sequence
---
+++ st_main.py
@@ -0,0 +1,114 @@
+import streamlit as st
+import pandas as pd
+import geopandas as gpd
+from scripts.find_node_sequence import find_node_sequence
+
+###################
+#### Funciones ####
+###################
+
+#find_node_sequence_to_all_routes: LineString Str GeoDataFrame Str Str GeoDataFrame Str Str -> DataFrame
+#Aplica el algoritmo find_node_sequence a todas las rutas contenidas en routes_gdf
+#Se obtiene un dataframe con la secuencia de nodos para cada una de las rutas
+def find_node_sequence_to_all_routes(nodes_gdf, node_id_col, 
+                                     edges_gdf, start_node_col, end_node_col,
+                                     routes_gdf, route_id_col, route_direction_col):
+    
+    #Cantidad total de rutas
+    n_routes = len(routes_gdf)
+
+    #Diccionario para almacenar secuencias de nodos
+    node_sequences_dict = {}
+
+    #Ciclo for para aplicar algoritmo a cada ruta por separado
+    for index, route_row in routes_gdf.iterrows():
+        
+        #Datos de la ruta
+        route_id = route_row[route_id_col] #Identificador único de la ruta
+        route_direction = route_row[route_direction_col] #Sentido de la ruta (NS o SN)        
+        linestring = route_row["geometry"] #Geometría de la ruta
+
+        #Aplicación Algoritmo
+        node_sequence = find_node_sequence(linestring, route_direction,
+                                           nodes_gdf, node_id_col,
+                                           edges_gdf, start_node_col, end_node_col)
+        
+        #Almacenaje de secuencia de nodos obtenida en el diccionario
+        node_sequences_dict[route_id] = node_sequence
+
+        #Actualización de la barra de progreso
+        progress = (index + 1 ) / n_routes
+        progress_bar.progress(progress, text = f"Buscando Secuencia de Nodos ({index+1}/{n_routes})") 
+
+    #Conversión de resultados de Diccionario a DataFrame
+    node_sequences_df = pd.DataFrame.from_dict(node_sequences_dict, orient='index')
+    node_sequences_df.columns = [f"NODO {i+1}" for i in range(node_sequences_df.shape[1])]
+    node_sequences_df.fillna(0, inplace=True)
+
+    #Almacenaje de Dataframe con resultado en variable de estado
+    st.session_state["node_sequences_df"] = node_sequences_df
+
+##################
+##### Inputs #####
+##################
+
+#Nodos
+nodes_path = "Inputs/Nodos/Nodos.shp"
+nodes_gdf = gpd.read_file(nodes_path)
+
+node_id_col = "ID"
+
+#Arcos
+edges_path = "Inputs/Arcos/Arcos.shp"
+edges_gdf = gpd.read_file(edges_path)
+
+start_node_col = "Nodo A"
+end_node_col = "Nodo B"
+
+#Rutas
+routes_path = "Inputs/Rutas/Rutas.shp"
+routes_gdf = gpd.read_file(routes_path)
+
+route_id_col = "Name"
+route_direction_col = "sentido"
+
+#############################
+#### Variables de Estado ####
+#############################
+
+if "node_sequences_df" not in st.session_state:
+    st.session_state["node_sequences_df"] = None
+
+
+##################
+#### Frontend ####
+##################
+
+# Título de la página
+st.title('Encontrar Secuencia de Nodos')
+
+# Subtítulo de Inputs
+st.header('Entradas')
+
+# División
+st.divider()
+
+#Botón para ejecutar el algoritmo
+
+find_node_sequence_button = st.button("Encontrar Secuencia de Nodos",
+                                      type = "primary",
+                                      on_click = find_node_sequence_to_all_routes,
+                                      args = (nodes_gdf, node_id_col, 
+                                              edges_gdf, start_node_col, end_node_col,
+                                              routes_gdf, route_id_col, route_direction_col))
+
+progress_bar = st.progress(0, text = "Buscando Secuencia de Nodos")
+progress_bar.empty()
+
+# División
+st.divider()
+
+# Subtítulo de Outputs
+st.header('Salidas')
+
+st.write(st.session_state["node_sequences_df"])
